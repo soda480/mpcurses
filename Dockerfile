60c03ce2849682ce0e44e3b83b60af234132ca42
Emilio Reyes <emilio.reyes@intel.com>
2021-09-24 15:23:36 -0700

    Upgrade to Python 3.9
    Signed-off-by: Emilio Reyes <emilio.reyes@intel.com>

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM python:3.6-alpine
+FROM python:3.9-slim
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV TERM xterm-256color
 WORKDIR /code
 COPY . /code/
-RUN pip install pybuilder==0.11.17
+RUN pip install pybuilder namegenerator
 RUN pyb install_dependencies
 RUN pyb install
--- build.py
+++ build.py
@@ -23,8 +23,9 @@ use_plugin('python.install_dependencies')
 use_plugin('python.flake8')
 use_plugin('python.coverage')
 use_plugin('python.distutils')
-use_plugin('pypi:pybuilder_radon', '~=0.1.2')
-use_plugin('pypi:pybuilder_bandit', '~=0.1.3')
+use_plugin('pypi:pybuilder_radon')
+use_plugin('pypi:pybuilder_bandit')
+use_plugin('pypi:pybuilder_anybadge')
 
 name = 'mpcurses'
 authors = [Author('Emilio Reyes', 'emilio.reyes@intel.com')]
@@ -34,8 +35,10 @@ version = '0.4.0'
 default_task = [
     'clean',
     'analyze',
+    'publish',
     'radon',
     'bandit',
+    'anybadge',
     'package']
 license = 'Apache License, Version 2.0'
 description = summary
@@ -66,7 +69,7 @@ def set_properties(project):
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: System :: Networking',
@@ -74,3 +77,5 @@ def set_properties(project):
     project.set_property('radon_break_build_average_complexity_threshold', 4)
     project.set_property('radon_break_build_complexity_threshold', 14)
     project.set_property('bandit_break_build', True)
+    project.set_property('anybadge_exclude', 'coverage, complexity')
+    project.set_property('anybadge_use_shields', True)

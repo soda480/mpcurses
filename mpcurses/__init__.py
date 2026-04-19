38be7199428e7f9c1f6789c2c8f3849f50822e96
Emilio Reyes <soda480@gmail.com>
2026-04-19 00:56:55 +0000

    build: update version
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- mpcurses/__init__.py
+++ mpcurses/__init__.py
@@ -37,7 +37,7 @@ def __getattr__(name):
 try:
     __version__ = _metadata.version(__name__)
 except _metadata.PackageNotFoundError:
-    __version__ = '0.9.0'
+    __version__ = '1.1.0'
 
 if getenv('DEV'):
     __version__ = f'{__version__}+dev'

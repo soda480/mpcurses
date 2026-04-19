8e8e648795e3407e158b39d54173b98a02e89b60
Emilio Reyes <soda480@gmail.com>
2026-04-19 00:46:37 +0000

    docs: update readme and demo
    Signed-off-by: Emilio Reyes <soda480@gmail.com>

--- docs/examples/demo.py
+++ docs/examples/demo.py
@@ -1,19 +1,26 @@
 from mpcurses import MPcurses
-import namegenerator, time, logging
+from time import sleep
+import logging
+import uuid
+import random
+
 logger = logging.getLogger(__name__)
 
-def do_something(*args):
-    for _ in range(0, 400):
-        logger.debug(f'processing item "{namegenerator.gen()}"')
-        time.sleep(.01)
+def do_work(worker_id=None, num_items=None):
+    logger.debug(f'worker {worker_id} will process {num_items} items')
+    for _ in range(num_items):
+        logger.debug(f'processing item "{uuid.uuid4()}"')
+        sleep(.01)
 
-MPcurses(
-    function=do_something,
-    screen_layout={
-        'display_item': {
-            'position': (1, 1), 'text': 'Processing:', 'text_color': 0, 'color': 14,
-            'clear': True, 'regex': r'^processing item "(?P<value>.*)"$'}
-    }).execute()
+def main():
+    MPcurses(
+        function=do_work,
+        process_data=[{'worker_id': f'Worker-{i}', 'num_items': random.randint(100, 200)} for i in range(3)],
+        screen_layout={
+            'worker': {'position': (1, 1), 'color': 2, 'table': True, 'clear': True, 'regex': r'^worker (?P<value>.*) will process \d+ items$'},
+            'num_items': {'position': (1, 10), 'color': 4, 'table': True, 'clear': True, 'regex': r'^worker .* will process (?P<value>\d+) items$'},
+            'item': {'position': (1, 14), 'color': 6, 'table': True, 'clear': True, 'regex': r'^processing item "(?P<value>.*)"$'},
+        }).execute()
 
 if __name__ == '__main__':
     main()
